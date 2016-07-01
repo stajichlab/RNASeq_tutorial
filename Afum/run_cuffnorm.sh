71b8f503f05cb1e3a8e44b4b75ed88f23473af03
Jason Stajich <jason@bioperl.org>
2016-07-01 14:17:09 -0700

    add labels

--- Afum/run_cuffnorm.sh
+++ Afum/run_cuffnorm.sh
@@ -8,5 +8,5 @@ fi
 CTL="cuffquant_SRR836437/abundances.cxb,cuffquant_SRR836438/abundances.cxb,cuffquant_SRR836439/abundances.cxb"
 EXP="cuffquant_SRR836440/abundances.cxb,cuffquant_SRR836441/abundances.cxb,cuffquant_SRR836442/abundances.cxb"
 
-cuffnorm genome/FungiDB-28_Afumigatus_Af293.gff -o Afum_cuffnorm $CTL $EXP
+cuffnorm genome/FungiDB-28_Afumigatus_Af293.gff -o Afum_cuffnorm -L Control,Treatment $CTL $EXP
 
