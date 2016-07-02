49db87d4d453c00c24a1ba0cd3cb010784c07216
Jason Stajich <jason@bioperl.org>
2016-07-02 16:13:02 -0700

    add some comments and help for non-UCR cluster using

--- Afum/02_download_fastq.sh
+++ Afum/02_download_fastq.sh
@@ -4,6 +4,9 @@ mkdir -p fastq
 cd fastq
 for acc in `awk '{print $1}' ../acc.txt`
 do
+# comment this out if you aren't on UCR system
 prefetch -a "/opt/linux/centos/7.x/x86_64/pkgs/aspera/3.6.0/bin/ascp|/opt/linux/centos/7.x/x86_64/pkgs/aspera/3.6.0/etc/asperaweb_id_dsa.openssh" $acc
+# use this if on mac or not on UCR system
+# prefetch $acc
 fastq-dump --skip-technical  --qual-filter-1 -F $acc
 done
